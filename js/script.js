6cc8890a5e555ca9bd2f2335ca837ff7e6668034
tebogo60 <tjjmalebana@gmail.com>
2023-02-10 13:10:09 +0200

    After clicking "menu-links a", side-menu closes.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
 // N A V I G A T I O N --------------------------------------------
 
 const body = document.querySelector(".body");
-
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
-
-// body.addEventListener("click", () => {
-
-// });
-window.addEventListener("scroll", () => {});
 
 let menuQpened = false;
 
@@ -30,6 +24,14 @@ const openMenu = () => {
 
 hamburger.addEventListener("click", () => {
   openMenu();
+});
+
+const menulink = document.querySelectorAll(".menu-links a");
+
+menulink.forEach((anchor) => {
+  anchor.addEventListener("click", () => {
+    openMenu();
+  });
 });
 
 const nav = document.querySelector(".nav");
@@ -57,29 +59,29 @@ window.addEventListener("scroll", () => {
   }
 });
 
-const lightMode = document.querySelector(".light-mode");
-const darkMode = document.querySelector(".dark-mode");
+// const lightMode = document.querySelector(".light-mode");
+// const darkMode = document.querySelector(".dark-mode");
 
-const modes = document.querySelector(".modes");
+// const modes = document.querySelector(".modes");
 
-let lightModeActive = false;
+// let lightModeActive = false;
 
-modes.addEventListener("click", () => {
-  if (lightModeActive === false) {
-    lightMode.classList.add("mode-active");
-    darkMode.classList.remove("mode-active");
-    body.classList.toggle("light-theme");
+// modes.addEventListener("click", () => {
+//   if (lightModeActive === false) {
+//     lightMode.classList.add("mode-active");
+//     darkMode.classList.remove("mode-active");
+//     body.classList.toggle("light-theme");
 
-    lightModeActive = true;
-    console.log(lightModeActive);
-    console.log(body);
-  } else {
-    lightMode.classList.remove("mode-active");
-    darkMode.classList.add("mode-active");
-    body.classList.toggle("light-theme");
+//     lightModeActive = true;
+//     console.log(lightModeActive);
+//     console.log(body);
+//   } else {
+//     lightMode.classList.remove("mode-active");
+//     darkMode.classList.add("mode-active");
+//     body.classList.toggle("light-theme");
 
-    lightModeActive = false;
-  }
-});
+//     lightModeActive = false;
+//   }
+// });
 
 // N A V I G A T I O N --------------------------------------------
